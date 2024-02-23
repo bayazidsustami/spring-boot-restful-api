f74c0289bb5e5a0dece0a6a53c4222dc13b1675b
bayazidsustami <bayazidsustamy@gmail.com>
2024-02-23 22:32:03 +0800

    initial commit

---
+++ src/test/java/com/example/restfullapi/demo/RestfullApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfullapi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
