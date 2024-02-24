aadfb4f1b1348d0ae87f68be54876bf3c0ac3a31
bayazidsustami <bayazidsustamy@gmail.com>
2024-02-24 11:17:08 +0800

    feat : create user controller

--- src/main/java/com/example/restfullapi/demo/service/UserService.java
+++ src/main/java/com/example/restfullapi/demo/service/UserService.java
@@ -1,12 +1,16 @@
 package com.example.restfullapi.demo.service;
 
+import com.example.restfullapi.demo.entity.User;
+import com.example.restfullapi.demo.exception.ApiException;
 import com.example.restfullapi.demo.model.RegisterUserRequest;
 import com.example.restfullapi.demo.repository.UserRepository;
+import com.example.restfullapi.demo.security.BCrypt;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
 
@@ -19,6 +23,7 @@ public class UserService {
     @Autowired
     private Validator validator;
 
+    @Transactional
     public void register(RegisterUserRequest request) {
         Set<ConstraintViolation<RegisterUserRequest>> constraintViolations = validator.validate(request);
         if (!constraintViolations.isEmpty()) {
@@ -26,9 +31,15 @@ public class UserService {
         }
 
         if (userRepository.existsById(request.getUsername())) {
-
+            throw new ApiException("username already registered");
         }
 
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(BCrypt.hashpw(request.getPassword(), BCrypt.gensalt()));
+        user.setName(request.getName());
+
+        userRepository.save(user);
     }
 
 }
